6a1f15f6393a4efee5d26506bfd4730a575bf7f4
Wilkin van Roosmalen <w.s.roosmalen@gmail.com>
2024-11-20 12:18:59 +0100

    Add shell.sh script and open qemu without graphics

--- start.sh
+++ start.sh
@@ -8,8 +8,9 @@ if [[ -z "$name" ]]; then
 fi
 
 if [ ! -d "data/$name" ]; then
-	echo "Data directory for $name (./data/$name/) does not exist. Run ./setup $name first"
-	exit 1;
+	echo "Data directory for $name (./data/$name/) does not exist. Running ./setup.sh $name first"
+	./setup.sh $name
+	echo "./setup.sh done"
 fi
 
 # Find available port
@@ -20,14 +21,21 @@ do
   continue
 done
 
+echo "ssh user@localhost -p $port -o StrictHostKeyChecking=no" > ./data/$name/ssh
+chmod +x ./data/$name/ssh
+
 echo "Starting VM $name at port $port"
-echo "For remote shell, run: ssh user@localhost -p $port"
+echo "For remote shell, execute: ./data/$name/ssh"
+echo "The VM will reboot once setup is complete!"
+
+sleep 10;
 
 qemu-system-x86_64 \
 	-m 2G \
-	-smp 2 \
+	-smp 1 \
 	-enable-kvm \
 	-machine accel=kvm:tcg \
+	-nographic \
 	-net nic -net user,hostfwd=tcp::"$port"-:22 \
 	-drive if=virtio,format=qcow2,file="./data/$name/img.cow" \
 	-drive media=cdrom,file="./data/$name/seed.iso"
